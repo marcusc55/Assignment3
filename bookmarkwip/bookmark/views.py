d706fedc2f46c0c097edd76df52282463c080dc7
marcusc55 <marcuschoo@outlook.com>
2014-08-11 15:30:24 +0000

    Second commit

--- bookmarkwip/bookmark/views.py
+++ bookmarkwip/bookmark/views.py
@@ -96,27 +96,32 @@ def add_category(request):
 def add_page (request, category_name_url):
       # Get the context from the request.
     context = RequestContext(request)
+    category_name=category_name_url.replace('_',' ')
 
     # A HTTP POST?
     if request.method == 'POST':
-        form = CategoryForm(request.POST)
+        form = PageForm(request.POST)
 
         # Have we been provided with a valid form?
         if form.is_valid():
             # Save the new category to the database.
-            form.save(commit=True)
+            page=form.save(commit=False)
+            page.category=Category.objects.get(name=category_name)
+            page.save()
+
+
 
             # Now call the index() view.
             # The user will be shown the homepage.
-            return index(request)
+            return category(request, category_name_url)
         else:
             # The supplied form contained errors - just print them to the terminal.
             print form.errors
     else:
         # If the request was not a POST, display the form to enter details.
-        form = CategoryForm()
+        form = PageForm()
 
     # Bad form (or form details), no form supplied...
     # Render the form with error messages (if any).
-    return render_to_response('bookmark/add_category.html', {'form': form}, context)
+    return render_to_response('bookmark/add_page.html', {'form': form, 'category_name_url':category_name_url}, context)
   
